61f217787c573a1a610bb7aa7b49ffd54d99c2a3
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-02-24 21:49:36 -0500

    create backend shell with dependencies

---
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/CardORamaBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.oswego.cs.CardORamaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardORamaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardORamaBackendApplication.class, args);
+	}
+
+}
---
+++ Card-O-Rama-Backend/src/test/java/edu/oswego/cs/CardORamaBackend/CardORamaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.oswego.cs.CardORamaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardORamaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
