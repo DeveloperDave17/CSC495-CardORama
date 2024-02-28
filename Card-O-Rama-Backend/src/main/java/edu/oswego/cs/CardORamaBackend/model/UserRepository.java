3a4f1daa7ce852babad5afe7a24f1d0a7482a75c
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-02-28 15:07:58 -0500

    create UserRepository

---
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/UserRepository.java
@@ -0,0 +1,7 @@
+package edu.oswego.cs.CardORamaBackend.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, String> {
+      
+}
