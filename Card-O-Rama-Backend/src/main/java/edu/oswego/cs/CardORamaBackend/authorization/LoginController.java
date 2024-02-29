e450a4cb0bfb38d732982d213e227984853b7a13
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-02-28 20:16:17 -0500

    add user creation upon successful first login

--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/authorization/LoginController.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/authorization/LoginController.java
@@ -1,21 +1,36 @@
 package edu.oswego.cs.CardORamaBackend.authorization;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.oswego.cs.CardORamaBackend.model.User;
+import edu.oswego.cs.CardORamaBackend.model.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
 
+   @Autowired
+   private UserRepository userRepository;
+
    @RequestMapping("/")
-   public void loginRedirect(HttpServletResponse httpServletResponse) {
+   public void loginRedirect(HttpServletResponse httpServletResponse, @AuthenticationPrincipal OAuth2User principal) {
       Authentication auth = SecurityContextHolder.getContext().getAuthentication();
       if (auth.isAuthenticated()) {
          httpServletResponse.setHeader("Location", "http://localhost:5173");
          httpServletResponse.setStatus(302);
+         String email = principal.getAttribute("email");
+         String name = principal.getAttribute("name");
+
+         if (!this.userRepository.findById(email).isPresent()) {
+            User user = new User(email, name);
+            this.userRepository.save(user);
+         }
       }
    }
 }
