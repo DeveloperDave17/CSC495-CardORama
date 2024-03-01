de4a81d6a12687a71f0c47cf80ca2a70b2afca8a
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-02-29 21:05:40 -0500

    create FlashcardRepository

---
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/flashcard/FlashcardRepository.java
@@ -0,0 +1,7 @@
+package edu.oswego.cs.CardORamaBackend.model.flashcard;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FlashcardRepository extends CrudRepository<Flashcard, Long> {
+   
+}
