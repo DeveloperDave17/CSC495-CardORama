e8f0cf7970a04190b92128ad1fed75d3f232accd
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-03 21:03:05 -0500

    fix jpa table reference issue

--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/flashcardset/FlashcardSetRepository.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/flashcardset/FlashcardSetRepository.java
@@ -14,18 +14,18 @@ public interface FlashcardSetRepository extends CrudRepository<FlashcardSet, Lon
    FlashcardSet findByEmailAndSetID(String email, Long setID);
 
    @Modifying
-   @Query("update FlashcardSets f set f.setName = :setName where f.setID = :setID")
+   @Query("update FlashcardSet f set f.setName = :setName where f.setID = :setID")
    void updateSetName(@Param(value = "setID") Long setID, @Param(value = "setName") String setName);
 
    @Modifying
-   @Query("update FlashcardSets f set f.privacy = :privacy where f.setID = :setID")
-   void updateSetPrivacy(@Param(value = "setID") Long setID, @Param(value = "privacy") FlashcardSetPrivacy flashcardSetPrivacy);
+   @Query("update FlashcardSet f set f.privacy = :privacy where f.setID = :setID")
+   void updatePrivacy(@Param(value = "setID") Long setID, @Param(value = "privacy") FlashcardSetPrivacy flashcardSetPrivacy);
 
    @Modifying
-   @Query("update FlashcardSets f set f.color = :color where f.setID = :setID")
-   void updateSetColor(@Param(value = "setID") Long setID, @Param(value = "color") String color);
+   @Query("update FlashcardSet f set f.color = :color where f.setID = :setID")
+   void updateColor(@Param(value = "setID") Long setID, @Param(value = "color") String color);
 
    @Modifying
-   @Query("update FlashcardSets f set f.priority = :priority where f.setID = :setID")
-   void updateSetPriority(@Param(value = "setID") Long setID, @Param(value = "priority") int priority);
+   @Query("update FlashcardSet f set f.priority = :priority where f.setID = :setID")
+   void updatePriority(@Param(value = "setID") Long setID, @Param(value = "priority") int priority);
 }
