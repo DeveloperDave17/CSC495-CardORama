eeba858870a724b8a63b80c582ac6f8a2b737ceb
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-06 10:01:33 -0500

    move FriendService to Services dir

--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/services/FriendService.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/services/FriendService.java
@@ -1,8 +1,11 @@
-package edu.oswego.cs.CardORamaBackend.model.friend;
+package edu.oswego.cs.CardORamaBackend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import edu.oswego.cs.CardORamaBackend.model.friend.Friend;
+import edu.oswego.cs.CardORamaBackend.model.friend.FriendRepository;
 
 @Service
 @Transactional
