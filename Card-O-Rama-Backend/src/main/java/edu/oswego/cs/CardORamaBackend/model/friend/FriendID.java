7184274e760efc51a52dfe03db1adfa04366ac61
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-06 10:09:53 -0500

    add removeFriendship Transaction

--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/friend/FriendID.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/friend/FriendID.java
@@ -5,4 +5,27 @@ import java.io.Serializable;
 public class FriendID implements Serializable {
    private String email;
    private String friendEmail;
+
+   public FriendID() {}
+
+   public FriendID(String email, String friendEmail) {
+      this.email = email;
+      this.friendEmail = friendEmail;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getFriendEmail() {
+      return friendEmail;
+   }
+
+   public void setFriendEmail(String friendEmail) {
+      this.friendEmail = friendEmail;
+   }
 }
--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/services/FriendService.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/services/FriendService.java
@@ -1,10 +1,13 @@
 package edu.oswego.cs.CardORamaBackend.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import edu.oswego.cs.CardORamaBackend.model.friend.Friend;
+import edu.oswego.cs.CardORamaBackend.model.friend.FriendID;
 import edu.oswego.cs.CardORamaBackend.model.friend.FriendRepository;
 
 @Service
@@ -20,6 +23,14 @@ public class FriendService {
       this.friendRepository.save(friend1);
       Friend friend2 = new Friend(email2, email1);
       this.friendRepository.save(friend2);
+   }
+
+   @Transactional
+   public void removeFriendship(String email1, String email2) {
+      FriendID friendID1 = new FriendID(email1, email2);
+      this.friendRepository.deleteById(friendID1);
+      FriendID friendID2 = new FriendID(email2, email1);
+      this.friendRepository.deleteById(friendID2);
    }
 
 }
