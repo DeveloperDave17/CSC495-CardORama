1a35ee5c133901a6d99e72e9dd877c13ca9d8fec
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-05 21:41:08 -0500

    create FriendRepository

---
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/friend/FriendRepository.java
@@ -0,0 +1,7 @@
+package edu.oswego.cs.CardORamaBackend.model.friend;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FriendRepository extends CrudRepository<Friend, FriendID> {
+   
+}
