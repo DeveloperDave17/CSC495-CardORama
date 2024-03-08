bc62a0cf34d0cc20728b1a3fe3f0fd8a940983ee
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-07 19:53:14 -0500

    update FriendRequestID to include setters and getters

--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/friendRequest/FriendRequestID.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/friendRequest/FriendRequestID.java
@@ -3,6 +3,23 @@ package edu.oswego.cs.CardORamaBackend.model.friendRequest;
 import java.io.Serializable;
 
 public class FriendRequestID implements Serializable {
+   
    private String email;
    private String friendEmail;
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getFriendEmail() {
+      return friendEmail;
+   }
+
+   public void setFriendEmail(String friendEmail) {
+      this.friendEmail = friendEmail;
+   }
 }
