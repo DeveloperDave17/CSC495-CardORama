92c389698d249594be6de61d85d73dcc25e6caca
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-07 20:13:30 -0500

    update FriendService to remove FriendRequests upon friendship creation

--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/services/FriendService.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/services/FriendService.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import edu.oswego.cs.CardORamaBackend.model.friend.Friend;
 import edu.oswego.cs.CardORamaBackend.model.friend.FriendID;
 import edu.oswego.cs.CardORamaBackend.model.friend.FriendRepository;
+import edu.oswego.cs.CardORamaBackend.model.friendRequest.FriendRequestID;
+import edu.oswego.cs.CardORamaBackend.model.friendRequest.FriendRequestRepository;
 
 @Service
 @Transactional
@@ -17,12 +19,21 @@ public class FriendService {
    @Autowired
    FriendRepository friendRepository;
 
+   @Autowired
+   FriendRequestRepository friendRequestRepository;
+
    @Transactional
    public void createFriendship(String email1, String email2) {
+      // Add users to each others friend requests.
       Friend friend1 = new Friend(email1, email2);
       this.friendRepository.save(friend1);
       Friend friend2 = new Friend(email2, email1);
       this.friendRepository.save(friend2);
+      // Remove pending friend requests for both users.
+      FriendRequestID possibleFriendRequest1 = new FriendRequestID(email1, email2);
+      this.friendRequestRepository.deleteById(possibleFriendRequest1);
+      FriendRequestID possibleFriendRequest2 = new FriendRequestID(email2, email1);
+      this.friendRequestRepository.deleteById(possibleFriendRequest2);
    }
 
    @Transactional
