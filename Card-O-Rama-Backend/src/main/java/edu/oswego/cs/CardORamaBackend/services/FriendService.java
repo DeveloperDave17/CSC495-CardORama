96bc9254be129abee44e3d130c99709906bdb3bc
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-08 22:31:34 -0500

    create sendFriendRequest in FriendService

--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/services/FriendService.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/services/FriendService.java
@@ -9,8 +9,10 @@ import org.springframework.transaction.annotation.Transactional;
 import edu.oswego.cs.CardORamaBackend.model.friend.Friend;
 import edu.oswego.cs.CardORamaBackend.model.friend.FriendID;
 import edu.oswego.cs.CardORamaBackend.model.friend.FriendRepository;
+import edu.oswego.cs.CardORamaBackend.model.friendRequest.FriendRequest;
 import edu.oswego.cs.CardORamaBackend.model.friendRequest.FriendRequestID;
 import edu.oswego.cs.CardORamaBackend.model.friendRequest.FriendRequestRepository;
+import edu.oswego.cs.CardORamaBackend.model.friendRequest.FriendRequestStatus;
 
 @Service
 @Transactional
@@ -42,6 +44,27 @@ public class FriendService {
       this.friendRepository.deleteById(friendID1);
       FriendID friendID2 = new FriendID(email2, email1);
       this.friendRepository.deleteById(friendID2);
+   }
+
+   @Transactional
+   public void sendFriendRequest(String userEmail, String friendReqEmail) {
+      Optional<Friend> friend = this.friendRepository.findById(new FriendID(userEmail, friendReqEmail));
+      // if the friendship doesn't exist
+      if (!friend.isPresent()) {
+         Optional<FriendRequest> friendRequestOptional = this.friendRequestRepository.findById(new FriendRequestID(userEmail, friendReqEmail));
+         if (friendRequestOptional.isPresent()) {
+            FriendRequest friendRequest = friendRequestOptional.get();
+            int friendRequestsSent = friendRequest.getRequestCount();
+            if (friendRequest.getFriendRequestStatus() == FriendRequestStatus.DECLINED && friendRequestsSent < 5) {
+               friendRequest.setFriendRequestStatus(FriendRequestStatus.PENDING);
+               friendRequest.setRequestCount(friendRequestsSent + 1);
+               this.friendRequestRepository.save(friendRequest);
+            }
+         } else {
+            FriendRequest friendRequest = new FriendRequest(userEmail, friendReqEmail);
+            this.friendRequestRepository.save(friendRequest);
+         }
+      }
    }
 
 }
