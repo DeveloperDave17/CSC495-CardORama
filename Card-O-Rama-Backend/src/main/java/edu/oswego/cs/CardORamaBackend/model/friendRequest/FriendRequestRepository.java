c82ffb27021cee1c7e40192f6083105c69499c2c
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-08 22:38:50 -0500

    create declineFriendRequest method

--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/friendRequest/FriendRequestRepository.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/friendRequest/FriendRequestRepository.java
@@ -1,7 +1,13 @@
 package edu.oswego.cs.CardORamaBackend.model.friendRequest;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface FriendRequestRepository extends CrudRepository<FriendRequest, FriendRequestID> {
    
+   @Modifying
+   @Query("update FriendRequest fr set fr.friendRequestStatus = DECLINED where fr.email = :email and fr.friendEmail = :friendEmail")
+   void declineFriendRequest(@Param(value = "email") String email, @Param(value = "friendEmail") String friendEmail);
 }
