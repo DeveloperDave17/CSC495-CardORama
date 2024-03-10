acf9b368a02297fe03653fb9b914880e9484cac5
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-10 12:38:04 -0400

    add query method getFriendRequestsList

--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/friendRequest/FriendRequestRepository.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/friendRequest/FriendRequestRepository.java
@@ -1,5 +1,7 @@
 package edu.oswego.cs.CardORamaBackend.model.friendRequest;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -10,4 +12,7 @@ public interface FriendRequestRepository extends CrudRepository<FriendRequest, F
    @Modifying
    @Query("update FriendRequest fr set fr.friendRequestStatus = DECLINED where fr.email = :senderEmail and fr.friendEmail = :email")
    void declineFriendRequest(@Param(value = "email") String email, @Param(value = "senderEmail") String senderEmail);
+
+   @Query("select fr from FriendRequest fr where fr.friendEmail = :userEmail and fr.friendRequestStatus = PENDING order by fr.email")
+   List<FriendRequest> getFriendRequestsList(@Param(value = "userEmail") String userEmail);
 }
