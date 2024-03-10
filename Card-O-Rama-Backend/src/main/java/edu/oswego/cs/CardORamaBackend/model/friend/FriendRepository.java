1192cd4fd0d5bd512dfe2d246b8aeeece8089432
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-10 12:29:32 -0400

    add findByEmailOrderByFriendEmail to provide a friends list query

--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/friend/FriendRepository.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/model/friend/FriendRepository.java
@@ -1,7 +1,10 @@
 package edu.oswego.cs.CardORamaBackend.model.friend;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface FriendRepository extends CrudRepository<Friend, FriendID> {
    
+   List<Friend> findByEmailOrderByFriendEmail(String email);
 }
