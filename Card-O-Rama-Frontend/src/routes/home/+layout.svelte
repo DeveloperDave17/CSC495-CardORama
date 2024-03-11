4c35f305542e8aa3ba8b731ce0bd910748f1ad0a
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-11 15:11:07 -0400

    create homepage

---
+++ Card-O-Rama-Frontend/src/routes/home/+layout.svelte
@@ -0,0 +1,124 @@
+<script>
+   let innerWidth = 0;
+   let menuDisplay = "none";
+   let menuDisplayImage = "/images/menu.svg";
+
+   function toggleHamburgerMenu() {
+      if (menuDisplay === "none") {
+         menuDisplay = "block";
+         menuDisplayImage = "/images/close.svg";
+      } else {
+         menuDisplay = "none";
+         menuDisplayImage = "images/menu.svg";
+      }
+   }
+</script>
+
+<svelte:window bind:innerWidth />
+
+<div id="nav-bar">
+   <div id="left-nav-bar">
+      <img id="nav-menu" src={menuDisplayImage} alt="selection menu" on:click={toggleHamburgerMenu}/>
+      <a class="nav-text-links">Edit</a>
+      <a class="nav-text-links">Guides</a>
+      <a class="nav-text-links">Study</a>
+   </div>
+   <div id="right-nav-bar">
+      <img id="nav-friends" src="/images/account-multiple.svg" alt="friends">
+      <img id="nav-plus" src="/images/plus.svg" alt="flashcard sets"/> 
+   </div>
+</div>
+{#if innerWidth < 769}
+   <div id="hamburger-selection-menu" style="display: {menuDisplay}">
+      <a>Edit</a>
+      <a>Guides</a>
+      <a>Study</a>
+      <a>Friends</a>
+   </div>
+{/if}
+
+<slot />
+
+<style>
+   @import "/static/styles/main.css";
+
+   :global(html) {
+      height: 100%;
+   }
+
+   :global(body) {
+      height: 100%;
+      margin: 0px;
+      display: flex;
+      flex-direction: column;
+   }
+
+   #nav-plus, #nav-menu, #nav-friends {
+      width: 64px;
+      height: 64px;
+   }
+
+   #nav-bar {
+      height: 100px;
+      background-color: var(--primary-bg-color);
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+   }
+
+   #nav-bar > div {
+      margin: 18px;
+   }
+
+   .nav-text-links {
+      color: var(--primary-fg-color);
+      font-size: 32px;
+   }
+
+   #nav-friends, .nav-text-links {
+      display: none;
+   }
+
+   #left-nav-bar, #right-nav-bar {
+      display: flex;
+   }
+
+   #left-nav-bar > *, #right-nav-bar > * {
+      margin-left: 40px;
+      margin-right: 40px;
+   }
+
+   #hamburger-selection-menu {
+      display: flex;
+      flex-direction: column;
+   }
+
+   #hamburger-selection-menu > a {
+      padding: 20px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 24px;
+      background-color: var(--primary-bg-color);
+      color: white;
+      border-style: solid;
+      border-width: 0 5px 5px 5px;
+      border-color: black;
+   }
+
+   #hamburger-selection-menu a:first-child {
+      border-width: 5px;
+   }
+
+
+
+   @media screen and (min-width: 769px) {
+      #nav-menu {
+         display: none;
+      }
+
+      #nav-friends, .nav-text-links {
+         display: block;
+      }
+   }
+</style>
--- Card-O-Rama-Frontend/src/routes/home/+page.svelte
+++ Card-O-Rama-Frontend/src/routes/home/+page.svelte
@@ -3,4 +3,97 @@
    import { GlobalReferences } from "$lib/globals";
 </script>
 
-<h1>Welcome to Card-O-Rama, </h1>
+<div id="homepage-content">
+   <div id="homepage-info">
+      <h1 id="homepage-title">Card-O-Rama</h1>
+      <p id="homepage-desc">Make flashcards and make connections!</p>
+   </div>
+   <div id="homepage-img-container">
+      <img id="homepage-img" src="/images/book-open-page-variant.svg" alt="open book">
+   </div> 
+   <div id="study-set-link-container">
+      <a id="study-set-link">My Flashcard Sets</a>
+   </div> 
+</div>
+
+<style>
+   @import "/static/styles/main.css";
+
+   #homepage-title, #homepage-desc {
+      color: var(--primary-bg-color);
+   }
+
+   #homepage-title {
+      font-size: 42px;
+      text-align: center;
+   }
+
+   #homepage-desc {
+      font-size: 32px;
+      text-align: center;
+   }
+
+   #homepage-img {
+      width: 306px;
+      height: 270px;
+   }
+
+   #homepage-content {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      height: 100%;
+   }
+
+   #homepage-info {
+      flex: 0 1 200px;
+      align-self: center;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      justify-content: center;
+   }
+
+   #homepage-img-container {
+      flex: 1;
+      align-self: center;
+      display: flex;
+      align-items: center;
+   }
+
+   #study-set-link-container {
+      flex: 0 1 250px;
+      align-self: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+   }
+
+   #study-set-link {
+      padding: 25px 125px;
+      font-size: 28px;
+      color: white;
+      background-color: var(--primary-bg-color);
+   }
+   
+   @media screen and (min-width: 769px) {
+      #homepage-img {
+         width: 381px;
+         height: 336px;
+      }
+
+      #study-set-link {
+         padding: 30px 175px;
+         font-size: 36px;
+      }
+
+      #homepage-title {
+         font-size: 48px;
+      }
+
+      #homepage-desc {
+         font-size: 38px;
+      }
+   }
+
+</style>
