3205f0a949cba4a31eaf1aef2ca44d6d060767d9
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-03-18 19:18:11 -0400

    create ReverseProxy

---
+++ Card-O-Rama-ReverseProxy/src/main/java/edu/oswego/cs/CardORamaReverseProxy/CardORamaReverseProxyApplication.java
@@ -0,0 +1,13 @@
+package edu.oswego.cs.CardORamaReverseProxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardORamaReverseProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardORamaReverseProxyApplication.class, args);
+	}
+
+}
---
+++ Card-O-Rama-ReverseProxy/src/test/java/edu/oswego/cs/CardORamaReverseProxy/CardORamaReverseProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.oswego.cs.CardORamaReverseProxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardORamaReverseProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
