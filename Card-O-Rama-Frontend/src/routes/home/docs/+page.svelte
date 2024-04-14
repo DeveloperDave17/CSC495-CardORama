9103def430473fbb6856492a4da822721dbc8a26
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-04-13 22:36:06 -0400

    add responsive animations and notifications

--- Card-O-Rama-Frontend/src/routes/home/docs/+page.svelte
+++ Card-O-Rama-Frontend/src/routes/home/docs/+page.svelte
@@ -1,6 +1,8 @@
 <script>
    import { GlobalReferences } from "$lib/globals";
    import { onMount } from "svelte";
+   import Swal from "sweetalert2";
+
    let globals = new GlobalReferences();
    function test() {
       fetch(globals.backendBasePath + `/GoogleDrive/info`, {
@@ -16,6 +18,7 @@
    }
 
    let pickerInited = false;
+   let loadingDisplay = "none";
 
    function onPickerApiLoad() {
       pickerInited = true;
@@ -55,12 +58,18 @@
          const document = data[google.picker.Response.DOCUMENTS][0];
          const fileId = document[google.picker.Document.ID];
          console.log(fileId);
+         loadingDisplay = "flex";
          fetch(globals.backendBasePath + `/GoogleDrive/loadFlashcardSet/${fileId}`, {
                mode: "same-origin",
                method: "post",
                credentials: "include",
             }).then((response) => {
                console.log(response.status);
+               loadingDisplay = "none";
+               Swal.fire({
+                  title: "Flashcard Set Creation Success!",
+                  icon: "success"
+               });
             })
       }
    }
@@ -75,7 +84,92 @@
       };
       googlePicker.src = "https://apis.google.com/js/api.js";
       document.head.appendChild(googlePicker);
-   })
+   });
 </script>
 
-<button on:click={openPicker}>Test</button>
+<div id="page">
+   <div id="page-content">
+      <img src="/images/drive_logo.png" alt="Google Drive Logo">
+      <p>Select a document to parse into flashcards. Make sure all of the desired terms and defintions are seperated
+         by the same deliminators. Allowed deliminators include colon(:), semi-colon(;), comma(,), hyphen(-), and is.
+         The colon symbol is recommended as it takes priority over other symbols.
+      </p>
+      <button on:click={openPicker}>Select Document</button>
+   </div>
+</div>
+<div id="loading-page" style="display: {loadingDisplay};">
+   <div id="loading-content">
+      <img id="loading-img" src="/images/loading.svg" alt="loading">
+   </div>
+</div>
+
+<style>
+   @import "/static/styles/main.css";
+
+
+   #page {
+      display: flex;
+      justify-content: center;
+   }
+
+   #page-content {
+      max-width: 800px;
+      min-width: 600px;
+      display: flex;
+      flex-direction: column;
+      margin-top: 100px;
+      justify-content: center;
+      align-items: center;
+      border: 2px solid var(--primary-bg-color);
+      padding: 25px;
+   }
+
+   p {
+      font-size: 28px;
+      color: var(--primary-bg-color);
+      font-weight: 500;
+      text-align: center;
+   }
+
+   button {
+      width: 400px;
+      height: 57px;
+      font-size: 28px;
+      background-color: var(--primary-bg-color);
+      color: white;
+   }
+
+   img {
+      width: 160px;
+   }
+
+   #loading-page {
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.6);
+      justify-content: center;
+      align-items: center;
+   }
+
+   #loading-content {
+      background-color: white;
+      box-sizing: border-box;
+      padding: 20px;
+      width: 200px;
+      height: 200px;
+   }
+
+   #loading-img {
+      animation: spin 1s linear infinite;
+   }
+
+   @keyframes spin {
+      0% {
+         transform: rotate(0deg);
+      }
+      100% {
+         transform: rotate(360deg);
+      }
+   }
+</style>
