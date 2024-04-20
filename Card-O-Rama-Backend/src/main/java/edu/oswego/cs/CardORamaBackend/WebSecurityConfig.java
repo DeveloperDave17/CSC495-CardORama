17ba7f0c5a67e5f5b676612a3826479be82f3e88
DeveloperDave17 <DavidHennigan17@gmail.com>
2024-04-19 22:22:30 -0400

    Modify all unauthorized requests to return 401 status and modify authorized logins to ONLY redirect to the default redirect

--- Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/WebSecurityConfig.java
+++ Card-O-Rama-Backend/src/main/java/edu/oswego/cs/CardORamaBackend/WebSecurityConfig.java
@@ -2,10 +2,12 @@ package edu.oswego.cs.CardORamaBackend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +16,10 @@ public class WebSecurityConfig {
    @Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/").permitAll().anyRequest().authenticated())
-      .csrf((csrf) -> csrf.disable()).oauth2Login(Customizer.withDefaults());
+      .sessionManagement((session) -> session.sessionFixation((sessionFixation) -> sessionFixation.newSession()))
+      .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+      .csrf((csrf) -> csrf.disable())
+      .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/", true));
       return http.build();
 	} 
 }
